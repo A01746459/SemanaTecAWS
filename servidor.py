39a741c749cbc59c96955af8e606aa45e00ee1c8
A01746459 <A01746459@itesm.mx>
2022-10-25 09:59:43 -0500

    actualizacion

--- servidor.py
+++ servidor.py
@@ -1,4 +1,10 @@
+from operator import methodcaller
 from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+from joblib import load
+from numpy import np
+#operaciones en el sistema
+import os
 
 #Generar el servidor en Flask (backend)
 servidorWeb = Flask(__name__)
@@ -7,6 +13,28 @@ servidorWeb = Flask(__name__)
 @servidorWeb.route("/test", methods=['GET'])
 def formulario():
     return render_template('pagina.html')
+
+#procesar datos a traves del form
+@servidorWeb.route("/modeloIA", methods=["POST"])
+def modeloForm():
+    #procesar datos de entrada
+    contenido = request.form
+    print(contenido)
+    #los json en python son diccionarios, lo convertimos 
+    return jsonify({"resultado":"datos recibidos"})
+
+#procesar datos dl archivo y guardarlos en la ruta del servidor, imprimir las lineas
+@servidorWeb.route("/modeloFile", methods=["POST"])
+def modeloFile():
+    f = request.files['file']
+    #trabajar con el file name pero usando otra biblioteca werkzug.utils
+    filename = secure_filename(f.filename)
+    path = os.path.join(os.getcwd,'files',filename)
+    f.save(path)
+    file = open(path,'r')
+    for line in file:
+        print(line)
+    return jsonify({"resultado":"datos recibidos"})
 
 if  __name__ == '__main__':
     servidorWeb.run(debug = False, host = '0.0.0.0', port = '8080')
